eeada427994c5ee65954225b1206ec60a07fa16e
Merrill Sequeira <59201447+consumer-git@users.noreply.github.com>
2021-04-09 13:27:50 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"darekarabhishek@gmail.com\"\n\
+email = \"merrill.sequeira@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
